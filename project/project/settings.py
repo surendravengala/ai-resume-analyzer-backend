039e0066edcec18437d03ec9b3d71aa1d6374ef2
Surendra Vengala <surendravengala2001@gmail.com>
2026-03-25 03:48:54 +0530

    fixed allowed hosts and debug

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rpn&^^7omafc&gfn$pw1@to_l6b4rtf0mkyzy$=wux=)t_a%z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
